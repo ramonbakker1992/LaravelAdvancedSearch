f97ac9cbd692bfa1e8afe961644954e6140ce4d4
ramonbakker1992 <ramon_bakker@msn.com>
2018-10-04 22:03:45 +0200

    Create Model.php

---
+++ Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Search\Searchable;
+
+class Task extends Model
+{
+	use Searchable;
+
+    public $searchColumns = [
+        'column_name' => 'where|betweenDates|checkedByDate',
+        'relation.column_name' => 'where|betweenDates|checkedByDate',
+    ];
+}
